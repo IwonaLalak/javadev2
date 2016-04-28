df0abdf42d40e1fae9d33af2f799297dd478bb07
IwonaLalak <lalak.iwona@gmail.com>
2016-04-28 13:28:03 +0200

    tworzenie studenta, oceny

---
+++ src/studentsANDgrades.java
@@ -0,0 +1,54 @@
+package com.pgs.javadev;
+
+/**
+ * Created by iwona on 28.04.16.
+ */
+public class studentsANDgrades
+{
+    int id;
+    String name;
+    String surname;
+
+    public studentsANDgrades(int id, String name, String surname)
+    {
+        this.id=id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSurname()
+    {
+        return surname;
+    }
+
+    public String getGrade()
+    {
+        return grades;
+    }
+
+    String grades=new String();
+
+    public boolean setGrade(int setting_grade)
+    {
+        if(setting_grade < 1 || setting_grade > 6)
+            return false;
+        this.grades +=String.valueOf(setting_grade)+" ";
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ID: "+id+" | name: "+name+" | surname: "+surname+" | grades: "+grades+" | \n";
+    }
+}
